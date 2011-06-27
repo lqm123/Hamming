f3662ec34383a0d350e5be4ebb7733826b22182d
Simon <kaltsimon@gmail.com>
2011-06-27 23:40:57 +0200

    runs a series of tests

---
+++ src/Aufgaben.java
@@ -0,0 +1,29 @@
+
+public class Aufgaben {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		HammingSimulator h = new HammingSimulator();
+		h.setData(new Matrix("p1.txt"));
+		h.doTransmission();
+		
+		System.out.println();
+		
+		h.setData(new Matrix("p2.txt"));
+		h.doTransmission();
+		
+		
+		for(int i = 1; i <= 3; i++) {
+			System.out.println();
+			
+			System.out.println("Received data: ");
+			h.received = new Matrix("a"+i);
+			h.received.printMatrixLine();
+			h.check();
+			h.decode();
+		}
+	}
+
+}
